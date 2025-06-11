660a0f7a1bb1de3620e66911dd9931fc868baae1
BonfaceGabriel <bonfacegabriel44@gmail.com>
2025-06-11 10:02:28 +0300

    feat: Add Seller and Product models to admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'seller', 'sku', 'price', 'inventory_count', 'is_active')
+    list_filter = ('is_active', 'seller')
+    search_fields = ('name', 'sku', 'seller__user__username')
--- sellers/admin.py
+++ sellers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SellerProfile
 
-# Register your models here.
+@admin.register(SellerProfile)
+class SellerProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'company_name', 'created_at')
+    search_fields = ('user__username', 'user__email', 'company_name')
