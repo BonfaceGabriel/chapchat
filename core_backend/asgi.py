9acdd5d58b4d6cec68427ff8476b6ac83a238fd2
BonfaceGabriel <bonfacegabriel44@gmail.com>
2025-06-19 16:45:59 +0300

    Reorder asgi.py

--- core_backend/asgi.py
+++ core_backend/asgi.py
@@ -1,11 +1,14 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_backend.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from core_backend.middleware import TokenAuthMiddleware
 import whatsapp_comms.routing
 
 # This line MUST be at the top, before other Django imports that need settings.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_backend.settings')
+
 
 # The 'application' variable must be defined at the end, after all necessary imports.
 application = ProtocolTypeRouter({
